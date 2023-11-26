9b47db4672166ce31869caff033f41afe63cb9ad
TommyRG84 <a_tomas.ruiz.gonzalez@iespablopicasso.es>
2023-11-26 13:30:12 +0100

    Añadimos la calse PruebaLavadora.java

---
+++ src/prog/unidad04a/atributos/ejercicio03/PruebaLavadora.java
@@ -0,0 +1,26 @@
+package prog.unidad04a.atributos.ejercicio03;
+
+import java.util.Scanner;
+
+public class PruebaLavadora {
+  
+//Colores
+ private static final String COLOR_BLANCO = "blanco";
+ private static final String COLOR_NEGRO = "negro";
+ private static final String COLOR_ROJO = "rojo";
+ private static final String COLOR_AZUL = "azul";
+ private static final String COLOR_GRIS = "gris";
+
+ /**
+  * Punto de entrada a la aplicación
+  * @param args
+  */
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("PROGRAMA MANEJO LAVADORAS");
+
+  }
+
+}
