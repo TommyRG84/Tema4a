e853dd0c4fe455d8e715c3f68227b874822de389
TommyRG84 <a_tomas.ruiz.gonzalez@iespablopicasso.es>
2023-11-26 12:27:33 +0100

    Añadimos la clase lavadora del ejercicio 3

---
+++ src/prog/unidad04a/atributos/ejercicio03/Lavadora.java
@@ -0,0 +1,70 @@
+//3. Crea una clase Lavadora con las siguientes características: Tiene un precio base,
+//un color (blanco, negro, rojo, azul y gris), calificación energética (letras entre la A y
+//la F, ambas incluidas), peso y carga máxima.
+
+package prog.unidad04a.atributos.ejercicio03;
+
+/**
+ * Representamos una lavadora
+ */
+
+public class Lavadora {
+  
+  /**
+   * Establecemos el precio base de la lavadora. Tiene que ser un double mayor que 0.
+   */
+  
+  public double precioBase;
+  
+  /**
+   * Establecemos el color de la lavadora. Tiene que ser blanco, negro, rojo azul o gris.
+   */
+  
+  public String color;
+  
+  /**
+   * Establecemos la calificación energética, tiene que estar comprendida entre la A y la F cinluidas.
+   *  
+   */
+  
+  public char calificacionEnergetica;
+  
+  /**
+   * Establecemos el peso de la lavadora, tiene que ser un número decimal positivo.
+   */
+  
+  public double peso;
+  
+  /**
+   * Establecemos la carga máxima de la lavadora, tiene que ser un número decimal positivo.
+   */
+  
+  public double cargaMaxima;
+  
+  /**
+   * Número total de lavadoras creadas-
+   */
+  
+  public static int lavadorasCreadas;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+}
