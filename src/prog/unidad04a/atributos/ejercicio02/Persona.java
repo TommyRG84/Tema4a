28347c71f47aa96d2ecf21d837ce6843425cc670
TommyRG84 <a_tomas.ruiz.gonzalez@iespablopicasso.es>
2023-11-26 10:32:07 +0100

    Creamos la clase persona para el ejercicio 2 de la primera relación

---
+++ src/prog/unidad04a/atributos/ejercicio02/Persona.java
@@ -0,0 +1,74 @@
+//2. Crea una clase Persona con los atributos: nombre, edad, DNI, sexo (H / M), peso
+//y altura. Haz pruebas con ella, como hemos visto en ejercicio anteriores.
+
+package prog.unidad04a.atributos.ejercicio02;
+
+/**
+ * Representa a una persona
+ */
+
+public class Persona {
+
+  /**
+   *  Nombre de la persona
+   */
+  
+  public String nombre;
+  
+  /**
+   * Edad de la persona. Debe de ser un número entero y estar comprendida entre 0 y 100.
+   */
+  
+  public int edad;
+  
+  /**
+   * DNI de la persona. Es una cadena de texto formada por 8 dígitos y una letra.
+   */
+  
+  public String dni;
+  
+  /**
+   * Sexo de la persona, debe de ser H o M.
+   */
+  
+  public char sexo;
+  
+  /**
+   * El peso de la persona. Puede ser un número decimal y mayor que cero.
+   */
+  
+  public double peso;
+  
+  
+  /**
+   * Altura de la persona. Es un double, tiene que ser mayor que cero.
+   */
+  
+  public double altura;
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
